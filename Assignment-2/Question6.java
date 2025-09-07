fc74bd7aa970321aa370dd8cfef2571517143629
suryakant-source <xbsuryakant@gmail.com>
2025-09-07 20:18:54 +0530

    Question6.java

---
+++ Assignment-2/Question6.java
@@ -0,0 +1,20 @@
+
+//	Q6. Write a program to check whether an inputted number is prime or not.
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                System.out.println(num + " is not a prime number");
+                return;
+            }
+        }
+        System.out.println(num + " is a prime number");
+    }
+}
