d03b25e2af17dbd071f5723ef63ca56f62bb4ab7
suryakant-source <xbsuryakant@gmail.com>
2025-09-07 20:06:47 +0530

    Question5.java

---
+++ Assignment-01/Question5.java
@@ -0,0 +1,17 @@
+//  Q5. Write a program to check a number is odd or even.
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even number");
+        } else {
+            System.out.println(num + " is odd number");
+        }
+    }
+}
