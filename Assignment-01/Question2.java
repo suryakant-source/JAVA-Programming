c8eb0c59a6c3a6e94ad0a921eff4b695eab608d7
suryakant-source <xbsuryakant@gmail.com>
2025-09-07 19:30:00 +0530

    Question2.java

---
+++ Assignment-01/Question2.java
@@ -0,0 +1,8 @@
+//  Q2. Write a program to display the addition result of two numbers 10.25 and 20.55 on the screen.
+
+class Main {
+    public static void main(String[] args) {
+        double x = 10.25, y = 20.55, res = x + y;
+        System.out.println("Addition of " + x + " and " + y + " is " + res);
+    }
+}
